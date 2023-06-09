6219fa385ddf21b0bbc12aee8d07c92d8c10a764
SuperGeeks Maceió <thiagogatinho537@gmail.com>
2023-06-09 17:30:23 -0300

    novedoseisdovinteetres

---
+++ src/components/baner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Baner = function(){
+    return(
+     <div>
+
+     </div>   
+    );
+}
+
+export default Baner;
+
---
+++ src/components/links.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Links = function(){
+    return(
+        <div>
+            <div class="container text-center">
+    <div class="row">
+        <div class="col">
+        <p><a href="#" class="link-primary">Primary link</a></p>
+            <p><a href="#" class="link-secondary">Secondary link</a></p>
+            <p><a href="#" class="link-success">Success link</a></p>
+            <p><a href="#" class="link-danger">Danger link</a></p>
+            <p><a href="#" class="link-warning">Warning link</a></p>
+            <p><a href="#" class="link-info">Info link</a></p>
+            <p><a href="#" class="link-light">Light link</a></p>
+            <p><a href="#" class="link-dark">Dark link</a></p>
+            <p><a href="#" class="link-body-emphasis">Emphasis link</a></p>
+        </div>
+        <div class="col order-5">
+       wiwiw
+        </div>
+        <div class="col order-1">
+              wewe  
+        </div>
+    </div>
+    </div>
+
+     </div>   
+    );
+}
+
+export default Links;
+
+
